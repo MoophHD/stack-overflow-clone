dc6c60391478d3579fd4d156de8da4cdca4b1a48
MoophHD <d_sizykh@mail.ru>
2020-11-05 20:01:41 +0300

    setup backend

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  res.send("API Running...")
+});
+
+const PORT = process.env.PORT || 3000;
+(async function () {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  } catch (e) {
+    console.log(`Server error, message: ${e}`);
+    process.exit(1);
+  }
+})();
