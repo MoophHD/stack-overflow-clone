c052fdff8e7768fef8ec6a826caa19d075d350b4
MoophHD <d_sizykh@mail.ru>
2020-11-09 18:14:30 +0300

    add auth middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  if (req.method == "OPTIONS") {
+    return next();
+  }
+
+  try {
+    // format: Bearer ${token}
+    const token = req.headers.authorization.split(" ")[1];
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+    }
+
+    next();
+  } catch (e) {
+    return res.status(401).json({ message: "No authorisation" });
+  }
+};
