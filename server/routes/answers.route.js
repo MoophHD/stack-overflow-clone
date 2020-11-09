62c5babc5fc9fbdf121b1a70bd475bbbd3300d3d
MoophHD <d_sizykh@mail.ru>
2020-11-09 18:48:08 +0300

    add answers route

---
+++ server/routes/answers.route.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Answer = require("../models/answer.model");
+const Question = require("../models/question.model");
+const auth = require("../middleware/auth.middleware");
+
+router.get("/:question_id", async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.question_id);
+
+    const answers = await Answer.find({ _id: { $in: question.answers } });
+    res.json({ answers });
+  } catch (e) {
+    res.status(500).json({ message: `Something went terribly wrong: ${e}` });
+  }
+});
+
+router.post("/", auth, async (req, res) => {
+  try {
+    const { questionId, text } = req.body;
+    const userId = req.user.id;
+    const answer = new Answer({ author: userId, questionId, text });
+    const savedAnswer = await answer.save();
+
+    const question = await Question.findById(questionId);
+    question.answers.push(savedAnswer.id);
+    await question.save();
+
+    res.json({ answer: savedAnswer });
+  } catch (e) {
+    res.status(500).json({ message: `Something went terribly wrong: ${e}` });
+  }
+});
+
+module.exports = router;
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use("/auth", require("./auth.route"));
 router.use("/users", require("./users.route"));
 router.use("/questions", require("./questions.route"));
+router.use("/answers", require("./answers.route"));
 
 module.exports = router;
