c52fd7f799640e3b9ad91beb72fde9a7dd07d05a
MoophHD <d_sizykh@mail.ru>
2020-11-23 13:14:27 +0300

    modify questions reducer to work with one question

--- client/src/redux/questions/questions.actions.js
+++ client/src/redux/questions/questions.actions.js
@@ -1,5 +1,25 @@
-import { GET_QUESTIONS_SUCCESS, GET_QUESTIONS_FAIL } from "./questions.types";
+import {
+  GET_QUESTIONS_SUCCESS,
+  GET_QUESTIONS_FAIL,
+  GET_QUESTION_FAIL,
+  GET_QUESTION_SUCCESS,
+} from "./questions.types";
 import axios from "axios";
+
+export const getQuestion = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/questions/${id}`);
+
+    dispatch({
+      type: GET_QUESTION_SUCCESS,
+      payload: res.data.question,
+    });
+  } catch (e) {
+    dispatch({
+      type: GET_QUESTION_FAIL,
+    });
+  }
+};
 
 export const getQuestions = () => async (dispatch) => {
   try {
--- client/src/redux/questions/questions.reducer.js
+++ client/src/redux/questions/questions.reducer.js
@@ -1,16 +1,25 @@
-import { GET_QUESTIONS_SUCCESS, GET_QUESTIONS_FAIL } from "./questions.types";
+import {
+  GET_QUESTIONS_SUCCESS,
+  GET_QUESTIONS_FAIL,
+  GET_QUESTION_SUCCESS,
+  GET_QUESTION_FAIL,
+} from "./questions.types";
 
 const initialState = {
   questions: [],
+  question: null,
   loading: true,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_QUESTION_SUCCESS:
+      return { ...state, question: action.payload, loading: false };
     case GET_QUESTIONS_SUCCESS:
       return { ...state, questions: action.payload, loading: false };
     case GET_QUESTIONS_FAIL:
-      return { ...state, questions: [], loading: false };
+    case GET_QUESTION_FAIL:
+      return { ...state, questions: [], loading: false, question: null };
     default:
       return state;
   }
