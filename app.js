5cd8509786c6295e280b49101117b746a6e22566
MoophHD <d_sizykh@mail.ru>
2020-11-27 20:18:10 +0300

    add scripts to package and add build resolver to server to host demo on heroku

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const index = require("./server/routes/index.route");
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api", index);
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "/client", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 3000;
 (async function () {
