00f2a3a35fbcfdabd89b7adb5314a1ae0ba25a0b
MoophHD <d_sizykh@mail.ru>
2020-12-14 20:47:51 +0300

    vote service, vote service unit tests

--- server/routes/votes.route.js
+++ server/routes/votes.route.js
@@ -1,76 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Question = require("../models/question.model");
-const Answer = require("../models/answer.model");
-const User = require("../models/user.model");
 const auth = require("../middleware/auth.middleware");
+const { upvote, downvote } = require("../controllers/vote.controller");
 
-function addVote(target, voteObj) {
-  target.score += voteObj.vote;
-  target.author.score += voteObj.vote;
-
-  target.votes.push(voteObj);
-}
-
-function removeVote(target, voteObj) {
-  target.score -= voteObj.vote;
-  target.author.score -= voteObj.vote;
-
-  target.votes.pull(voteObj);
-}
-
-async function vote(target, voteValue, voterId) {
-  const existingVote = target.votes.find((v) => v.user._id.equals(voterId));
-  const sameVote = existingVote && existingVote.vote === voteValue;
-
-  // cancel vote
-  if (sameVote) {
-    removeVote(target, existingVote);
-  } else if (existingVote) {
-    removeVote(target, existingVote);
-    addVote(target, { user: voterId, vote: voteValue });
-  } else {
-    addVote(target, { user: voterId, vote: voteValue });
-  }
-}
-
-async function buildVote(req, res, voteValue) {
-  let target;
-  if (req.params.answer_id) {
-    target = await Answer.findById(req.params.answer_id).populate({
-      path: "author",
-      select: "firstName lastName score",
-    });
-  } else {
-    target = await Question.findById(req.params.question_id).populate({
-      path: "author",
-      select: "score",
-    });
-  }
-
-  const voter = req.user.id;
-
-  await vote(target, voteValue, voter);
-  await target.save();
-  await target.author.save();
-
-  res.json({ target });
-}
-
-router.get("/upvote/:question_id/:answer_id?", auth, async (req, res) => {
-  try {
-    await buildVote(req, res, 1);
-  } catch (e) {
-    res.status(500).json({ message: `Something went terribly wrong: ${e}` });
-  }
-});
-
-router.get("/downvote/:question_id/:answer_id?", auth, async (req, res) => {
-  try {
-    await buildVote(req, res, -1);
-  } catch (e) {
-    res.status(500).json({ message: `Something went terribly wrong: ${e}` });
-  }
-});
+router.get("/upvote/:question_id/:answer_id?", auth, upvote);
+router.get("/downvote/:question_id/:answer_id?", auth, downvote);
 
 module.exports = router;
---
+++ server/tests/vote.test.js
@@ -0,0 +1,113 @@
+const AnswerModel = require("../models/answer.model");
+const QuestionModel = require("../models/question.model");
+const UserModel = require("../models/user.model");
+const VoteService = require("../services/vote.service");
+const { setupDb, clearDb } = require("../tests/setup");
+
+beforeEach(async () => {
+  await setupDb();
+  await QuestionModel.ensureIndexes();
+  await AnswerModel.ensureIndexes();
+  await UserModel.ensureIndexes();
+});
+
+afterEach(async () => {
+  await clearDb();
+});
+
+describe("Vote Service", () => {
+  let user, question, answer;
+
+  beforeEach(async () => {
+    user = await new UserModel({
+      firstName: "Aaron",
+      lastName: "Burr",
+      email: "qwe@mail.ru",
+      password: "qwe100",
+    }).save();
+
+    question = await new QuestionModel({
+      author: user._id,
+      text: "Description",
+      title: "Title",
+    }).save();
+
+    answer = await new AnswerModel({
+      author: user._id,
+      text: "Answer text",
+    }).save();
+
+    question.answers.push(answer);
+    await question.save();
+    // create one question and several answers from the data provided above
+  });
+
+  it("Should increment Question score on upvote", async () => {
+    await VoteService.voteQuestion(question._id, user._id, 1);
+    const updatedQuestion = await QuestionModel.findById(question._id);
+    expect(updatedQuestion.score).toBe(1);
+  });
+
+  it("Should increment Answer on upvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, 1);
+    const updatedAnswer = await AnswerModel.findById(answer._id);
+
+    expect(updatedAnswer.score).toBe(1);
+  });
+
+  it("Should decrement Question score on downvote", async () => {
+    await VoteService.voteQuestion(question._id, user._id, -1);
+    const updatedQuestion = await QuestionModel.findById(question._id);
+
+    expect(updatedQuestion.score).toBe(-1);
+  });
+
+  it("Should decrement Answer on downvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+    const updatedAnswer = await AnswerModel.findById(answer._id);
+
+    expect(updatedAnswer.score).toBe(-1);
+  });
+
+  it("Should increment User score on question upvote ", async () => {
+    await VoteService.voteQuestion(question._id, user._id, 1);
+    const updatedUser = await UserModel.findById(user._id);
+
+    expect(updatedUser.score).toBe(1);
+  });
+
+  it("Should increment User score on answer upvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, 1);
+    const updatedUser = await UserModel.findById(user._id);
+
+    expect(updatedUser.score).toBe(1);
+  });
+
+  it("Should decrement User score on question downvote ", async () => {
+    await VoteService.voteQuestion(question._id, user._id, -1);
+    const updatedUser = await UserModel.findById(user._id);
+
+    expect(updatedUser.score).toBe(-1);
+  });
+
+  it("Should decrement User score on answer downvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+    const updatedUser = await UserModel.findById(user._id);
+
+    expect(updatedUser.score).toBe(-1);
+  });
+
+  it("Should cancel question/answer vote and nulify gained score on double upvote/downvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+
+    expect(user.score).toBe(0);
+  });
+
+  it("Should cancel user vote and nulify gained score on double upvote/downvote ", async () => {
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+    await VoteService.voteAnswer(answer._id, user._id, -1);
+
+    expect(user.score).toBe(0);
+  });
+});
